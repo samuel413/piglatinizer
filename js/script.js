2e2f0a4f1da494e9de7e2d56c445591ef8021928
samuel413 <sapereira@s.sfusd.edu>
2019-10-01 01:29:11 +0000

    added a to do list, set up the phrase splitting, need to do words split

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $( "document" ).ready(function() {
-
-
-
+$(".submit").click(function(){
+    var startPhrase = $(".text").val();
+    var words = startPhrase.split(' ')
+        words.forEach(function(word){
+            firstLetter = word[0];
+        }
+});
 });
