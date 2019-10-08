308139a998faaf5d0454d24808c7b273169178ec
samuel413 <sapereira@s.sfusd.edu>
2019-10-08 01:11:29 +0000

    i think its done but it wont preview

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $( "document" ).ready(function() {
 $(".submit").click(function(){
+    var vowels = ['a','e','i','o','u']
+    var finalString = ''
     var startPhrase = $(".text").val();
     var words = startPhrase.split(' ')
-        words.forEach(function(word){
-            firstLetter = word[0];
-        }
+    words.forEach(function(word){
+        var firstLetter = word[0];
+            var restofword = word.split(1);
+        if( $.inArray(firstLetter, vowels) != -1){
+            finalString.append(word+'way'+' ')
+        } else {
+            finalString.append(restofword+firstLetter+'ay'+' ')
+        };
+    });
+    $('.result').text(finalString)
 });
 });
